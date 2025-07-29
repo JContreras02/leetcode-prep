08334ec2c6ebc6ffccbed9916cf15726fc2da526
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-28 23:58:58 -0400

    solved two sum

--- 1_easy/two_sum.py
+++ 1_easy/two_sum.py
@@ -1,10 +1,18 @@
 # -------Brute Force Attempt-------- O(n^2)
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+# def two_sum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
 
-print(two_sum([2, 3, 11, 7], 9))
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        tracker = {}
 
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in tracker:
+                return [tracker[diff], i]
+            tracker[n] = i
 
